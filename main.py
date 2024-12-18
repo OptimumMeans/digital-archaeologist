a51831fecbb046003588fcf0d7c35fedda2869f1
OptimumMeans <cameron@cozyfiles.us>
2024-12-17 21:43:16 -0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from flask import Flask, jsonify
 from generate_facts import get_current_artifact
-from flask_cors import CORS  # Add CORS support
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
+CORS(app)
 
 @app.route('/')
 def home():
     return "Digital Archaeologist API is running!"
 
-
 @app.route('/api/artifact')
 def get_artifact():
     return jsonify(get_current_artifact())
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
